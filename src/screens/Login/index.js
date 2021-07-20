981c9f5de7dcb632560ed2d7c57195f453b9424e
Naluisa <anaalutorres@gmail.com>
2021-07-19 23:55:01 -0300

    Estilizando tela de login

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text } from 'react-native';
-import { Container } from './styles';
+import { Container,
+InputArea,
+CustomButton,
+CustomButtonText,
+LoginMensagemButton,
+LoginMensagemButtonText,
+LoginMensagemButtonTextBold } from './styles';
+
+import Componente from '../../assets/Componente.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
+
+import LoginInput from '../../components/LoginInput';
+
 
 export default () => {
+
+    const [emailField, setEmailField] = useState('');
+    const [passwordField, setPasswordField] = useState('');
+
     return(
         <Container>
-            <Text>Login</Text>
+            <Componente width="100%" height="160"/>
+
+            <InputArea>
+                <LoginInput 
+                    IconSvg={EmailIcon}
+                    placeholder="Digite seu e-mail"
+                    value={emailField}
+                    onChangeText={t=>setEmailField(t)}
+                />
+
+                <LoginInput 
+                    IconSvg={LockIcon}
+                    placeholder="Digite sua senha"
+                    value={passwordField}
+                    onChangeText={t=>setEmailField(t)}
+                    password={true}
+                />
+
+                <CustomButton>
+                    <CustomButtonText>LOGIN</CustomButtonText>
+                </CustomButton>
+
+            </InputArea>
+
+            <LoginMensagemButton>
+                <LoginMensagemButtonText>Ainda não possui uma conta?</LoginMensagemButtonText>
+                <LoginMensagemButtonTextBold>Cadastre-se</LoginMensagemButtonTextBold>
+            </LoginMensagemButton>
+
         </Container>
     );
 }
